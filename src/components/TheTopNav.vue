c7fc675d6b5c6b5daf4e984dcee5ff49b7480f8f
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 09:46:46 +0200

    add raw top bar

---
+++ src/components/TheTopNav.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar flat>
+    <v-toolbar app title="Shop">
+      <v-btn icon>
+        <v-icon small>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar>
+  </v-app-bar>
+</template>
