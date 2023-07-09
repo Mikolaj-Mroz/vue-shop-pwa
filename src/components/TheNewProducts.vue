b9423bbf85c4c93c54b4208c886f882e6cf773c1
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 18:50:27 +0200

    feat: wishlist products from homepage

--- src/components/TheNewProducts.vue
+++ src/components/TheNewProducts.vue
@@ -27,6 +27,22 @@
                       ></v-progress-circular>
                     </v-row>
                   </template>
+
+                  <template>
+                    <!--Add, or remove wishlist button-->
+                  </template>
+                  <v-btn
+                    @click="changeWishlist(product)"
+                    size="x-small"
+                    flat
+                    icon
+                    :ripple="false"
+                    color="transparent"
+                    style="position: absolute; right: 4px; bottom: 4px"
+                  >
+                    <v-icon size="x-large" color="red">{{wishlist.wishlist.includes(product)? 'mdi-heart':'mdi-heart-outline' }}</v-icon>
+                  </v-btn>
+
                 </v-img>
             </v-row>
             <v-row no-gutters class="mt-2 text-capitalize text-caption" style="line-height:0.8rem">
@@ -48,19 +64,17 @@
 <script setup>
 import { ref } from 'vue'
 import { useProductsStore } from '../stores/products'
+import { useWishlistStore } from '../stores/products'
 
 const productsStore = useProductsStore()
 
 import { Swiper, SwiperSlide } from 'swiper/vue'
-
 import 'swiper/css'
 
 const products = productsStore.products
 
 const filtered = ref(products)
-
 const filter = ref(null)
-
 const filtersList = ['For Him', 'For Her', 'For Kids']
 
 const filterProducts = () => {
@@ -69,6 +83,16 @@ const filterProducts = () => {
     filtered.value = products.filter(product => product.category === filterx)
   } else {
     filtered.value = products
+  }
+}
+
+const wishlist = useWishlistStore()
+
+const changeWishlist = (product) => {
+  if (wishlist.wishlist.includes(product)) {
+    wishlist.removeFromWishlist(product)
+  } else {
+    wishlist.addToWishlist(product)
   }
 }
 
