3e8461f9e5f2d5ce51c71938e3b2731d311b974f
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 16:02:01 +0200

    feat: add edge case when cart is empty

--- src/views/CartView.vue
+++ src/views/CartView.vue
@@ -1,4 +1,11 @@
 <template>
+  <h2 class="text-h6 font-weight-bold">Cart</h2>
+  <v-divider class="my-4"></v-divider>
+  <v-row v-if="store.cart.length === 0" align="center" justify="center">
+    <v-col cols="12" class="text-center">
+      <span class="text-h6">Your cart is empty</span>
+    </v-col>
+  </v-row>
   <CartProduct
     v-for="product in store.cart"
     :product="product"
