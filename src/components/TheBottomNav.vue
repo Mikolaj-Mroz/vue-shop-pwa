80f99fb9083c2f360e4058ee798939ccc533c5d5
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 09:46:16 +0200

    add bottom nav bar

---
+++ src/components/TheBottomNav.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-layout class="overflow-visible" style="height: 56px">
+    <v-bottom-navigation active color="primary">
+      <v-btn>
+        <v-icon>mdi-home-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-heart-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-cart-outline</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <v-icon>mdi-account-outline</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-layout>
+</template>
