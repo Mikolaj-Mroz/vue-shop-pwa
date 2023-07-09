674518875fc70cd7e1ac568449ce359c55b442e6
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 09:47:21 +0200

    add simple skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TheTopNav from './components/TheTopNav.vue';
+import TheBottomNav from './components/TheBottomNav.vue'
 </script>
 
 <template>
-  <RouterView />
+  <v-app>
+    <TheTopNav />
+    <v-main>
+      <RouterView />
+    </v-main>
+    <TheBottomNav />
+  </v-app>
 </template>
