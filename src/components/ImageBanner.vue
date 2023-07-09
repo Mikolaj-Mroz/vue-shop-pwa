fef4b0f761818c86211b6aed4ae2aa57130ab645
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-09 10:39:34 +0200

    add image banner

---
+++ src/components/ImageBanner.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-sheet
+    :color="color"
+  >
+    <v-img 
+      :src="image"
+      cover
+      :height="200"
+      @click="$router.push('/featured')"
+    >
+      <div 
+        class="text-center font-weight-bold text-body-1"
+        style="position: absolute; bottom: 4px; width: 100%"
+      >
+        {{ text }}
+      </div>
+    </v-img>
+  </v-sheet>
+</template>
+
+<script setup>
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({
+  text: {
+    type: String,
+    default: ''
+  },
+  color: {
+    type: String,
+    default: 'primary'
+  },
+  image: {
+    type: String,
+    default: ''
+  },
+  button: {
+    type: Boolean,
+    default: false
+  }
+})
+
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,17 @@
     icon="mdi-information-outline"
     backgroundColor="warning"
   />
+
+  <ImageBanner 
+    text="Our choice for this season"
+    class="mt-12"
+    image="/homepage/banner-1-photo.jpg"
+  />
 </template>
 
 <script setup>
 import BannerComponent from '../components/BannerComponent.vue';
+import ImageBanner from '../components/ImageBanner.vue';
 
 
 </script>
