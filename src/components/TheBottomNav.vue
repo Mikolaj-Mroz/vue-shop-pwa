fa7aecff030bfc812b6a91d88932f1719cf274a2
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-12 16:06:47 +0200

    feat: change route animation

--- src/components/TheBottomNav.vue
+++ src/components/TheBottomNav.vue
@@ -2,18 +2,52 @@
   <v-bottom-navigation tag="footer" active color="primary">
     <v-btn to="/">
       <v-icon>mdi-home-outline</v-icon>
+      <div v-if="route.path === '/'" class="active"></div>
     </v-btn>
 
     <v-btn to="/wishlist">
       <v-icon>mdi-heart-outline</v-icon>
+      <div v-if="route.path === '/wishlist'" class="active"></div>
     </v-btn>
 
     <v-btn to="/cart">
       <v-icon>mdi-cart-outline</v-icon>
+      <div v-if="route.path === '/cart'" class="active"></div>
     </v-btn>
 
     <v-btn to="/account">
       <v-icon>mdi-account-outline</v-icon>
+      <div v-if="route.path === '/account'" class="active"></div>
     </v-btn>
   </v-bottom-navigation>
 </template>
+
+<script setup>
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+console.log(route.path)
+</script>
+
+<style scoped>
+
+@keyframes active-animation {
+  0% {
+    transform: scaleX(0);
+  }
+  100% {
+    transform: scaleX(1);
+  }
+}
+  
+.active {
+  position: absolute; 
+  top:0;
+  width:100%; 
+  height:2px; 
+  background-color: rgb(var(--v-theme-primary));
+  transform-origin: left center;
+  animation-name: active-animation;
+  animation-duration: 0.3s;
+}
+</style>
