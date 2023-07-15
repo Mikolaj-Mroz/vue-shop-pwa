400c330da31401d7fe992dc537e67b31f1e7bb70
Mikolaj-Mroz <s223884@student.uek.krakow.pl>
2023-07-15 15:02:01 +0200

    feat: add scss, change font + few styling choices

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import './assets/style.scss'
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
